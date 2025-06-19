1a68857fbec056af0c98d351ba5bf076a594c4c4
Dylan Johnson <dylanatasmsa@gmail.com>
2025-06-19 10:04:21 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "My system flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+        overlays = [ ];
+      };
+    in
+      {
+        nixosConfigurations = {
+          djohnson-thinkpad-nixos = nixpkgs.lib.nixosSystem {
+            specialArgs = {
+              inherit pkgs;
+              inherit system;
+            };
+            modules = [
+              ./configurations/configuration.nix
+            ];
+          };
+        };
+      };
+}
