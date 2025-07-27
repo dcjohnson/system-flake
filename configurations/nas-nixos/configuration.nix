a0b339f8b183bd74d175ee56df56cad30c84c61c
Dylan Johnson <dylanatasmsa@gmail.com>
2025-07-27 02:20:30 -0700

    fix

--- configurations/nas-nixos/configuration.nix
+++ configurations/nas-nixos/configuration.nix
@@ -43,11 +43,11 @@
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  services.xserver.enable = false;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = false;
+  services.xserver.desktopManager.gnome.enable = false;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -86,12 +86,11 @@
       "wheel"
     ];
     packages = with pkgs; [
-      #  thunderbird
     ];
   };
 
   # Install firefox.
-  programs.firefox.enable = true;
+  programs.firefox.enable = false;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -99,8 +98,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
+    nvim
+    wget
+    curl
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
