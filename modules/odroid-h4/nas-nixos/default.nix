e7f74ad54aeab7920916ffe6eb6ec39c2abd4c27
Dylan Johnson <dylanatasmsa@gmail.com>
2025-08-09 10:13:55 -0700

    cleanup

--- modules/odroid-h4/nas-nixos/default.nix
+++ modules/odroid-h4/nas-nixos/default.nix
@@ -15,6 +15,11 @@
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
 
+    nix.settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
     networking.hostName = "nixos"; # Define your hostname.
     networking.wireless.enable = false; # Enables wireless support via wpa_supplicant.
 
--- pkgs/odroid-h4-plus-nas-installer/install-odroid-os.sh
+++ pkgs/odroid-h4-plus-nas-installer/install-odroid-os.sh
@@ -5,16 +5,19 @@ parted /dev/nvme0n1 -- mkpart ESP fat32 1MB 512MB
 parted /dev/nvme0n1 -- mkpart root btrfs 512MB 100%
 parted /dev/nvme0n1 -- set 1 esp on
 
-
-
 mkfs.fat -F 32 -n boot /dev/nvme0n1p1       
 mkfs.btrfs -f -L nixos /dev/nvme0n1p2
+
 sleep 5
+
 mount /dev/disk/by-label/nixos /mnt
 mkdir /boot                   
 mount -o umask=077 /dev/disk/by-label/boot /boot
 nixos-generate-config --root /mnt --flake 
-nixos-install --flake github:dcjohnson/system-flake#odroid-h4-nas-v1-default
+nix build github:dcjohnson/system-flake#odroid-h4.nas-v1.default
+nixos-installer --system ./result
+
+#nixos-install --flake github:dcjohnson/system-flake#odroid-h4-nas-v1-default
 
 # set the fstab
 cat << EOF > /etc/fstab
