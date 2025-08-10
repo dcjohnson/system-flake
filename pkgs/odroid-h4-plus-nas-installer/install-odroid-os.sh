4b93e76c754af82b0875eec5ff0eced7c42847ac
Dylan Johnson <dylanatasmsa@gmail.com>
2025-08-09 19:54:45 -0700

    fix

--- pkgs/odroid-h4-plus-nas-installer/install-odroid-os.sh
+++ pkgs/odroid-h4-plus-nas-installer/install-odroid-os.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 
-nix run 'github:nix-community/disko/latest#disko-install' -- --write-efi-boot-entries --flake 'github:dcjohnson/system-flake#odroid-h4-nas' disk nvme0n1 /dev/nvme0n1 
+nix run 'github:nix-community/disko/latest#disko-install' -- --write-efi-boot-entries --flake 'github:dcjohnson/system-flake#odroid-h4-nas' --disk nvme0n1 /dev/nvme0n1 
 
 mount /dev/nvme0n1p2 /mnt
 
