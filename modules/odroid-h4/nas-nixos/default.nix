124246b51a0c2f6da0b48c9fe3d6548eb6ecd36b
Dylan Johnson <dylanatasmsa@gmail.com>
2025-08-13 11:56:50 -0700

    update

--- modules/odroid-h4/nas-nixos/default.nix
+++ modules/odroid-h4/nas-nixos/default.nix
@@ -12,8 +12,12 @@
 
   config = {
     # Bootloader.
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+    boot = {
+      loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+      };
+    };
 
     nix.settings.experimental-features = [
       "nix-command"
