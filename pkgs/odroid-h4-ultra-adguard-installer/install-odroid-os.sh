fe5b5511f745cc61c965f5411603e5ac849d5f5b
Dylan Johnson <dylanatasmsa@gmail.com>
2025-11-27 15:26:05 -0800

    new pkg

---
+++ pkgs/odroid-h4-ultra-adguard-installer/install-odroid-os.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash 
+
+nix run 'github:nix-community/disko/latest#disko-install' -- --write-efi-boot-entries --flake 'github:dcjohnson/system-flake#odroid-h4-adguard' --disk nvme0n1 /dev/nvme0n1
+
+mount /dev/nvme0n1p2 /mnt
+
+echo 'Set root password'
+nixos-enter --root /mnt -c 'passwd root'
+echo 'Set trader password'
+nixos-enter --root /mnt -c 'passwd adguard'
+
+umount /dev/nvme0n1p2
