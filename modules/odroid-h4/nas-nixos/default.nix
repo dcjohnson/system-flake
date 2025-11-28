39c97eae617ecc4c12854e1111602e2b0c86e84a
Dylan Johnson <dylanatasmsa@gmail.com>
2025-11-28 09:14:17 -0800

    smb

--- modules/odroid-h4/nas-nixos/default.nix
+++ modules/odroid-h4/nas-nixos/default.nix
@@ -47,6 +47,24 @@
       };
     };
 
+    services.samba = {
+      enable = true;
+      settings = {
+        global = {
+          "usershare path" = "/var/lib/samba/usershares";
+          "usershare max shares" = "100";
+          "usershare allow guests" = "yes";
+          "usershare owner only" = "no";
+        };
+      };
+      openFirewall = true;
+    };
+
+    services.samba-wsdd = {
+      enable = true;
+      openFirewall = true;
+    };
+
     services.zfs = {
       autoScrub.enable = true;
       autoSnapshot.enable = true;
