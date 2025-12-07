540e4482cf39689b90a4c985503f9e83b760bda9
Dylan Johnson <dylanatasmsa@gmail.com>
2025-12-06 16:08:14 -0800

    update

--- modules/lenovo-thinkpad-t470/default.nix
+++ modules/lenovo-thinkpad-t470/default.nix
@@ -32,12 +32,12 @@
     # Enable the X11 windowing system.
     services.xserver.enable = true;
 
-    services.xserver.displayManager.gdm = {
+    services.displayManager.gdm = {
       enable = true;
       wayland = true;
     };
     services.displayManager.defaultSession = "gnome";
-    services.xserver.desktopManager.gnome.enable = true;
+    services.desktopManager.gnome.enable = true;
     services.rpcbind.enable = true;
 
     # Configure keymap in X11
