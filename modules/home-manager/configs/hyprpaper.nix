514f23a1cd4e9ee81eb9529887124330b088cba0
Dylan Johnson <dylanatasmsa@gmail.com>
2025-12-12 06:24:08 -0800

    other configs

---
+++ modules/home-manager/configs/hyprpaper.nix
@@ -0,0 +1,13 @@
+{
+  ipc = "on";
+  splash = false;
+  splash_offset = 2.0;
+
+  preload = [
+    "/home/dcj/.config/hyprpaper/nixos.png"
+  ];
+
+  wallpaper = [
+    ",/home/dcj/.config/hyprpaper/nixos.png"
+  ];
+}
--- modules/home-manager/dcj.nix
+++ modules/home-manager/dcj.nix
@@ -5,128 +5,16 @@
   home.stateVersion = "24.11"; # Comment out for error with "latest" version
   programs = {
     home-manager.enable = true;
-    ashell = {
-      enable = true;
-      systemd = {
-        enable = true;
-      };
-      settings = {
-        log_level = "warn";
-        outputs = {
-          Targets = [ "eDP-1" ];
-        };
-        position = "Top";
-        app_launcher_cmd = "hyprlauncher";
-
-        modules = {
-
-          left = [
-            [
-              "appLauncher"
-              "Workspaces"
-            ]
-          ];
-          center = [ "WindowTitle" ];
-          right = [
-            "SystemInfo"
-            [
-              "Tray"
-              "Clock"
-              "Privacy"
-              "Settings"
-            ]
-          ];
-        };
-
-        system_info = builtins.fromTOML ''
-          indicators = [
-                      "Cpu",
-                      "Memory",
-                      "MemorySwap",
-                      {Disk = "/" },
-                      "UploadSpeed",
-                      "DownloadSpeed",
-                    ]'';
-
-        CustomModule = [
-          {
-            name = "appLauncher";
-            icon = "󱗼";
-            command = "hyprlauncher";
-          }
-        ];
-
-        workspaces = {
-          enable_workspace_filling = true;
-        };
-
-        window_title = {
-          truncate_title_after_length = 100;
-        };
-
-        settings = {
-          lock_cmd = "playerctl --all-players pause; nixGL hyprlock &";
-          audio_sinks_more_cmd = "pavucontrol -t 3";
-          audio_sources_more_cmd = "pavucontrol -t 4";
-          wifi_more_cmd = "nm-connection-editor";
-          vpn_more_cmd = "nm-connection-editor";
-          bluetooth_more_cmd = "blueman";
-        };
-
-        appearance = {
-          style = "Islands";
-
-          primary_color = "#7aa2f7";
-          success_color = "#9ece6a";
-          text_color = "#a9b1d6";
-          workspace_colors = [
-            "#7aa2f7"
-            "#9ece6a"
-          ];
-          special_workspace_colors = [
-            "#7aa2f7"
-            "#9ece6a"
-          ];
-
-          danger_color = {
-            base = "#f7768e";
-            weak = "#e0af68";
-          };
-
-          background_color = {
-            base = "#1a1b26";
-            weak = "#24273a";
-            strong = "#414868";
-          };
-
-          secondary_color = {
-            base = "#0c0d14";
-          };
-        };
-      };
-    };
+    ashell = import ./configs/ashell.nix;
   };
   wayland.windowManager.hyprland = {
     systemd.enable = false;
     enable = true;
     settings = import ./configs/hyprland.nix;
   };
+  services.pasystray.enable = true;
   services.hyprpaper = {
     enable = true;
-    #settings = {
-    #  ipc = "on";
-    #  splash = false;
-    #  splash_offset = 2.0;
-#
-#      preload = [
-#        "/share/wallpapers/buttons.png"
-#        "/share/wallpapers/cat_pacman.png"
-#      ];
-#
-#      wallpaper = [
-#        "DP-3,/share/wallpapers/buttons.png"
-#        "DP-1,/share/wallpapers/cat_pacman.png"
-#      ];
-#    };
+    settings = import ./configs.hyprpaper.nix;
   };
 }
