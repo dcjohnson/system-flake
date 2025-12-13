b005bcbd262c4e3770a6b14c17db50e1a490d062
Dylan Johnson <dylanatasmsa@gmail.com>
2025-12-13 07:52:04 -0800

    update

--- modules/home-manager/dcj.nix
+++ modules/home-manager/dcj.nix
@@ -4,7 +4,12 @@
   home.homeDirectory = "/home/dcj";
   home.stateVersion = "24.11"; # Comment out for error with "latest" version
   programs = {
-    home-manager.enable = true;
+    home-manager = {
+      enable = true;
+    };
+    nm-applet = {
+      enable = true;
+    };
     ashell = import ./configs/ashell.nix;
   };
   wayland.windowManager.hyprland = {
@@ -12,9 +17,13 @@
     enable = true;
     settings = import ./configs/hyprland.nix;
   };
-  services.pasystray.enable = true;
-  services.hyprpaper = {
-    enable = true;
-    settings = import ./configs/hyprpaper.nix;
+  services = {
+    pasystray = {
+      enable = true;
+    };
+    hyprpaper = {
+      enable = true;
+      settings = import ./configs/hyprpaper.nix;
+    };
   };
 }
