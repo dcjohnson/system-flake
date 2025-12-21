d326d73cf7f0412b7142e0f761f1017a572b5c47
Dylan Johnson <dylanatasmsa@gmail.com>
2025-12-20 16:28:44 -0800

    update

--- modules/system76-thelio-inspired/configuration.nix
+++ modules/system76-thelio-inspired/configuration.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [
-    # Include the results of the hardware scan.
+    ../hyprland/hyprland.nix
     ./hardware-configuration.nix
   ];
 
@@ -93,11 +93,6 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
   services.rpcbind.enable = true;
 
   # Configure keymap in X11
@@ -125,18 +120,6 @@
     rtkit = {
       enable = true;
     };
-  };
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
