c40d8606be01fc9ae9299c4e6fee9cc785b00df0
Dylan Johnson <dylanatasmsa@gmail.com>
2025-12-24 08:02:35 -0800

    update

--- modules/home-manager/configs/hyprpaper.nix
+++ modules/home-manager/configs/hyprpaper.nix
@@ -4,13 +4,10 @@
   splash_offset = 2.0;
 
   preload = [
-    "/home/djohnson/.config/wallpapers/seagull.jpg"
     "/home/djohnson/.config/wallpapers/nixos.png"
-    "/home/djohnson/.config/wallpapers/big-sur-sunset.jpg"
   ];
 
   wallpaper = [
-    "HDMI-A-2,/home/djohnson/.config/wallpapers/nixos.png"
-    "DP-5,/home/djohnson/.config/wallpapers/nixos.png"
+    ",/home/dcj/.config/wallpapers/nixos.png"
   ];
 }
