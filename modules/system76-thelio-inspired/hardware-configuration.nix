68a8b48c6c50f456545de890f763e87f81744b92
Dylan Johnson <dylanatasmsa@gmail.com>
2025-12-27 16:55:45 -0800

    update

--- modules/system76-thelio-inspired/hardware-configuration.nix
+++ modules/system76-thelio-inspired/hardware-configuration.nix
@@ -55,5 +55,13 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
 }
