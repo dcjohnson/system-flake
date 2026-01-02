f081af6c3e65fd24559ff3169e9209398edf97bf
Dylan Johnson <dylanatasmsa@gmail.com>
2026-01-01 16:13:30 -0800

    update wallpaper

--- modules/home-manager/configs/hyprpaper.nix
+++ modules/home-manager/configs/hyprpaper.nix
@@ -4,11 +4,12 @@
   splash_offset = 2.0;
 
   preload = [
+    "/home/dcj/.config/wallpapers/mountain_night_sky-wallpaper-3840x1200.jpg"
     "/home/dcj/.config/wallpapers/blue_mountains_mist-wallpaper-5120x1600.jpg"
     "/home/dcj/.config/wallpapers/nixos.png"
   ];
 
   wallpaper = [
-    ",/home/dcj/.config/wallpapers/blue_mountains_mist-wallpaper-5120x1600.jpg"
+    ",/home/dcj/.config/wallpapers/mountain_night_sky-wallpaper-3840x1200.jpg"
   ];
 }
